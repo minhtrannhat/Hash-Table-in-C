bb073674dbed29e747f88f7f6fa5f9e71254ab18
minhradz <minhradz@gmail.com>
2020-03-31 17:10:43 -0400

    deleted unnecessary comment

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,11 +1,9 @@
-// key-value pairs stored in a struct
 typedef struct
 {
     char * key;
     char * value;
 } ht_items;
 
-// hash table stores an array of pointers to items, and some details about its size and how full it is 
 typedef struct
 {
     int size;
