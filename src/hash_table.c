5896273397028c94ffc5ae73f69f32ae0cd8bfb5
minhradz <minhradz@gmail.com>
2020-04-03 19:12:56 -0400

    The searching function is similar to the insert function but each iteration of the loop , we check whether the item's key matches the key we're searching for.

--- src/hash_table.c
+++ src/hash_table.c
@@ -99,3 +99,25 @@ void ht_insert(ht_hash_table * ht, const char * key, const char * value)
     ht -> items[index] = item;
     ht -> count++;
 }
+
+char * ht_search(ht_hash_table * ht, const char * key)
+{
+    int index = ht_get_hash(key, ht -> size, 0);
+
+    ht_items * item = ht -> items[index];
+
+    int i = 1;
+    while(item != NULL)
+    {
+        if(strcmp(item -> key, key) == 0)
+        {
+            return item -> value;
+        }
+
+        index = ht_get_hash(key, ht -> size , i);
+        item = ht -> items[index];
+        i++;
+    }
+
+    return NULL;
+}
