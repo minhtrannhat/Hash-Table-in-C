5a11d9cb34580f1093b5c9939730e72d7150b122
minhradz <minhradz@gmail.com>
2020-12-13 21:31:53 -0500

    Fix: moved all functions prototypes to the correct folder

--- src/hash_table.h
+++ src/hash_table.h
@@ -11,3 +11,10 @@ typedef struct {
 
 ht_hash_table *ht_new();
 void ht_del_hash_table(ht_hash_table *ht);
+static ht_item *ht_new_item(const char *k, const char *v);
+static void ht_del_item(ht_item *i);
+static ht_item *ht_new_item(const char *k, const char *v);
+static int ht_get_hash(const char *s, const int num_buckets, const int attempt);
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *ht, const char *key);
